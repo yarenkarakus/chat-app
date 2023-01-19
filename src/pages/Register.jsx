352639d3a7ae1dba41b55365cb03a24701ae0856
Yaren Karakus <yarenkarakus007@gmail.com>
2023-01-19 19:17:32 +0300

    register page created

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,19 +1,32 @@
 import React from "react";
+import Add from "../img/addAvatar.png";
 
 const Register = () => {
   return (
     <div className="bg-indigo-300 h-screen flex items-center justify-center">
-      <div className="bg-white py-5 px-14 rounded-lg flex flex-col gap-2.5">
-        <span>Lama Chat</span>
-        <span>Register</span>
-        <form className="flex flex-col gap-4">
+      <div className="bg-white py-5 px-14 rounded-lg flex flex-col gap-2.5 items-center ">
+        <span className="text-indigo-800 font-bold text-2xl">Gossip Chat</span>
+        <span className="text-indigo-800  text-xl">Register</span>
+        <form className="flex flex-col gap-4 ">
           <input type="text" placeholder="display name" />
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
-          <input type="file" />
-          <button className="bg-gray-100">Sign up</button>
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label
+            className="flex items-center gap-2.5 text-indigo-200 text-xs cursor-pointer"
+            htmlFor="file"
+          >
+            <img className="w-8" src={Add} alt="Add" />
+            <span> Add an avatar</span>
+          </label>
+
+          <button className="bg-indigo-500 text-white p-2.5 font-bold cursor-pointer rounded-lg hover:bg-indigo-400">
+            Sign up
+          </button>
         </form>
-        <p>You do have an account? Login </p>
+        <p className="text-indigo-800  text-xs mt-2.5">
+          You do have an account? Login{" "}
+        </p>
       </div>
     </div>
   );
