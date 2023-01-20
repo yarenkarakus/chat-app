afdd4d3bc50d7d0a67ed11fb9de92f5d436f992e
Yaren Karakus <yarenkarakus007@gmail.com>
2023-01-20 15:17:05 +0300

    components created

---
+++ src/components/Chat.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Chat = () => {
+  return <div className="flex-1 w-32">Chat</div>;
+};
+
+export default Chat;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sidebar = () => {
+  return <div className="flex-1 w-64">Sidebar</div>;
+};
+
+export default Sidebar;
