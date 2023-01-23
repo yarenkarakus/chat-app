efa5732586305ace1a3dd3de9494fa763edda5a7
Yaren Karakus <yarenkarakus007@gmail.com>
2023-01-23 13:58:42 +0300

    fixed pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,20 +2,18 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className="bg-indigo-300 h-screen flex items-center justify-center">
-      <div className="bg-white py-5 px-14 rounded-lg flex flex-col gap-2.5 items-center ">
-        <span className="text-indigo-800 font-bold text-2xl">Gossip Chat</span>
-        <span className="text-indigo-800  text-xl">Login</span>
+    <div className="bg-zinc-200  h-screen flex items-center justify-center">
+      <div className="bg-cool-grey-050 py-5 px-14 rounded-lg flex flex-col gap-2.5 items-center ">
+        <h1 className=" font-bold text-2xl">Gossip Chat</h1>
+        <h3 className="  text-xl">Login</h3>
         <form className="flex flex-col gap-4 ">
-          <input type="email" placeholder="email" />
-          <input type="password" placeholder="password" />
-          <button className="bg-indigo-500 text-white p-2.5 font-bold cursor-pointer rounded-lg hover:bg-indigo-400">
+          <input className="input" type="email" placeholder="email" />
+          <input className="input" type="password" placeholder="password" />
+          <button className="bg-indigo-600 text-white p-2.5 font-bold cursor-pointer rounded-lg hover:bg-indigo-400">
             Sign in
           </button>
         </form>
-        <p className="text-indigo-800  text-xs mt-2.5">
-          You don't have an account? Register{" "}
-        </p>
+        <p className=" text-xs mt-2.5">You don't have an account? Register </p>
       </div>
     </div>
   );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,30 +3,35 @@ import Add from "../img/addAvatar.png";
 
 const Register = () => {
   return (
-    <div className="bg-indigo-300 h-screen flex items-center justify-center">
-      <div className="bg-white py-5 px-14 rounded-lg flex flex-col gap-2.5 items-center ">
-        <span className="text-indigo-800 font-bold text-2xl">Gossip Chat</span>
-        <span className="text-indigo-800  text-xl">Register</span>
+    <div className=" h-screen flex items-center justify-center">
+      <div className="bg-cool-grey-050  py-5 px-14 rounded-xl flex flex-col gap-2.5 items-center ">
+        <h1 className="  font-bold text-2xl">Gossip Chat</h1>
+        <h3 className="  text-xl">Register</h3>
         <form className="flex flex-col gap-4 ">
-          <input type="text" placeholder="display name" />
-          <input type="email" placeholder="email" />
-          <input type="password" placeholder="password" />
-          <input style={{ display: "none" }} type="file" id="file" />
+          <input className="input" type="text" placeholder="Name" />
+          <input className="input" type="email" placeholder="Email" />
+          <input className="input" type="password" placeholder="Password" />
+          <input
+            className="input"
+            style={{ display: "none" }}
+            type="file"
+            id="file"
+          />
           <label
-            className="flex items-center gap-2.5 text-indigo-200 text-xs cursor-pointer"
+            className="flex items-center gap-2.5  text-xs cursor-pointer"
             htmlFor="file"
           >
-            <img className="w-8" src={Add} alt="Add" />
+            <img className="w-10" src={Add} alt="Add" />
             <span> Add an avatar</span>
           </label>
 
-          <button className="bg-indigo-500 text-white p-2.5 font-bold cursor-pointer rounded-lg hover:bg-indigo-400">
+          <button className="bg-indigo-600 flex items-center justify-center gap-1 text-white p-2.5 font-bold cursor-pointer rounded-lg hover:bg-indigo-400 ">
+            <div className="w-4 h-4 border-b-2 border-b-white animate-spin rounded-full"></div>
             Sign up
           </button>
         </form>
-        <p className="text-indigo-800  text-xs mt-2.5">
-          You do have an account? Login{" "}
-        </p>
+
+        <p className=" text-xs mt-2.5">You do have an account? Login </p>
       </div>
     </div>
   );
