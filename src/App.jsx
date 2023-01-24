1910532dd5b78b45f5c3d2c31e18dd2de5d8c600
Yaren Karakus <yarenkarakus007@gmail.com>
2023-01-24 17:40:32 +0300

    wip: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
-  return <Register />;
+  return <Home />;
 }
 
 export default App;
