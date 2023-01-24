afbe825487949d3f028ccf1dd98fdd1b6cb46517
Yaren Karakus <yarenkarakus007@gmail.com>
2023-01-24 17:43:03 +0300

    wip: home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Chat from "../components/Chat";
 const Home = () => {
   return (
     <div className=" h-screen flex items-center justify-center">
-      <div className="border-2 border-solid overflow-hidden border-white rounded-xl w-2/3 h-4/5 flex">
+      <div className="border-2 border-solid overflow-hidden border-white rounded-xl w-2/3 h-4/5 flex ">
         <Sidebar />
         <Chat />
       </div>
