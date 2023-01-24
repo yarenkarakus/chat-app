abecfa052d454f3fbd1bfb0e0a22beab8834f796
Yaren Karakus <yarenkarakus007@gmail.com>
2023-01-24 17:44:25 +0300

    feat: message text and img

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message flex gap-5 mb-5 owner">
+      <div className="flex flex-col text-cool-grey-300 font-light">
+        <img
+          className="w-10 h-10 rounded-full object-cover"
+          src="https://images.pexels.com/photos/14270561/pexels-photo-14270561.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+          alt=""
+        />
+        <span>Just Now</span>
+      </div>
+      <div className="messagecontent max-w-[80%] flex flex-col gap-2.5 items-end">
+        <p className="bg-white max-w-max py-2.5 px-5 rounded-xl rounded-tl-none">
+          Hello
+        </p>
+        <img
+          className="w-2/4"
+          src="https://images.pexels.com/photos/14270561/pexels-photo-14270561.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
