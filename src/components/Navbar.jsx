d76ee856bfc639870522cb10600f93ba5c940585
Yaren Karakus <yarenkarakus007@gmail.com>
2023-01-24 19:57:57 +0300

    refactor: font style changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="text-white flex items-center bg-indigo-900 h-12 p-2.5 justify-between">
-      <span className="font-bold">Gossip Chat</span>
+      <span className="font-bold gossip">Gossip Chat</span>
       <div className="flex gap-2.5">
         <img
           src="https://images.pexels.com/photos/14270561/pexels-photo-14270561.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
