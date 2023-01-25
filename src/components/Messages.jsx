14556c5f6dc2038669832382d8631fd5e8ddbf9f
Yaren Karakus <yarenkarakus007@gmail.com>
2023-01-25 13:11:13 +0300

    fix: scrollbar

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -3,7 +3,7 @@ import Message from "./Message";
 
 const Messages = () => {
   return (
-    <div className="bg-cool-grey-100 p-2.5 h-[calc(100%-100px)] overflow-scroll ">
+    <div className="bg-cool-grey-100 p-2.5 h-[calc(100%-100px)] overflow-y-scroll scrool-cool-grey-050 ">
       <Message />
       <Message />
       <Message />
