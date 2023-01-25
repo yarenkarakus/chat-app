a9d8cb38ca7ce6820a0af583fd38576c24329267
Yaren Karakus <yarenkarakus007@gmail.com>
2023-01-25 13:10:59 +0300

    refactor: width changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,8 @@ import Chat from "../components/Chat";
 
 const Home = () => {
   return (
-    <div className=" h-screen flex items-center justify-center">
-      <div className="border-2 border-solid overflow-hidden border-white rounded-xl w-2/3 h-4/5 flex ">
+    <div className=" h-screen  flex items-center justify-center">
+      <div className="border-2  border-solid overflow-hidden border-white rounded-xl w-3/5 h-4/5 flex ">
         <Sidebar />
         <Chat />
       </div>
