fb91d78d6a64ad84e62d59a738894f97186771c1
Yaren Karakus <yarenkarakus007@gmail.com>
2023-01-26 13:31:45 +0300

    refactor: profile photo and displayname changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <div className="text-white flex items-center bg-indigo-900 h-12 p-2.5 justify-between">
       <span className="font-bold gossip">Gossip Chat</span>
       <div className="flex gap-2.5">
         <img
-          src="https://images.pexels.com/photos/14270561/pexels-photo-14270561.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+          src={currentUser.photoURL}
           alt=""
           className="bg-cool-grey-050 h-6 w-6 rounded-full object-cover "
         />
-        <span>John</span>
-        <button className="bg-orange-vivid-700 p-1 rounded-xl text-white text-xs border-none cursor-pointer">
+        <span>{currentUser.displayName}</span>
+        <button
+          className="bg-orange-vivid-700 p-1 rounded-xl text-white text-xs border-none cursor-pointer"
+          onClick={() => signOut(auth)}
+        >
           Logout
         </button>
       </div>
