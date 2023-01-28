c958078e55e6f75fdc586a65b29744a22c428de1
Yaren Karakus <yarenkarakus007@gmail.com>
2023-01-28 18:08:07 +0300

    feat: message memory refactor: img src changed

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { useContext, useEffect, useRef } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
-const Message = () => {
+const Message = ({ message }) => {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
+
   return (
-    <div className="message flex gap-5 mb-5 owner">
+    <div
+      ref={ref}
+      className={`message ${message.senderId === currentUser.uid && "owner"}`}
+    >
       <div className="flex flex-col text-cool-grey-300 font-light">
         <img
           className="w-10 h-10 rounded-full object-cover"
-          src="https://images.pexels.com/photos/14270561/pexels-photo-14270561.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.user.photoURL
+          }
           alt=""
         />
         <span>Just Now</span>
       </div>
       <div className="messagecontent max-w-[80%] flex flex-col gap-2.5 items-end">
         <p className="bg-white max-w-max py-2.5 px-5 rounded-xl rounded-tl-none">
-          Hello
+          {message.text}
         </p>
-        <img
-          className="w-2/4"
-          src="https://images.pexels.com/photos/14270561/pexels-photo-14270561.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
-          alt=""
-        />
+        {message.img && <img className="w-2/4" src={message.img} alt="" />}
       </div>
     </div>
   );
