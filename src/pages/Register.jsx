46d1a9f22bc6e0f323607e94d4d427cbea4e0213
Yaren Karakus <yarenkarakus007@gmail.com>
2023-01-30 15:03:14 +0300

    feat: profile photos view

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,6 +9,7 @@ import { useNavigate, Link } from "react-router-dom";
 const Register = () => {
   const [err, setErr] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [image, setImage] = useState(null);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -22,19 +23,17 @@ const Register = () => {
     try {
       const res = await createUserWithEmailAndPassword(auth, email, password);
 
-      const storageRef = ref(storage, `${displayName + date}`);
-
       const date = new Date().getTime();
+      const storageRef = ref(storage, `${displayName + date}`);
 
       await uploadBytesResumable(storageRef, file).then(() => {
         getDownloadURL(storageRef).then(async (downloadURL) => {
           try {
-            //Update profile
             await updateProfile(res.user, {
               displayName,
               photoURL: downloadURL,
             });
-            //create user on firestore
+
             await setDoc(doc(db, "users", res.user.uid), {
               uid: res.user.uid,
               displayName,
@@ -42,7 +41,6 @@ const Register = () => {
               photoURL: downloadURL,
             });
 
-            //create empty user chats on firestore
             await setDoc(doc(db, "userChats", res.user.uid), {});
             navigate("/");
           } catch (err) {
@@ -58,42 +56,72 @@ const Register = () => {
     }
   };
 
+  const handleImage = (e) => {
+    const target = e.target;
+    const file = target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        const base64 = reader.result;
+        if (typeof base64 === "string") {
+          setImage(base64);
+        }
+      };
+    }
+  };
+
   return (
     <div className=" h-screen flex items-center justify-center">
-      <div className="bg-cool-grey-050  py-5 px-14 rounded-xl flex flex-col gap-2.5 items-center ">
-        <h1 className="  font-bold text-2xl">Gossip Chat</h1>
-        <h3 className="  text-xl">Register</h3>
-        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-          <input className="input" type="text" placeholder="Name" />
-          <input className="input" type="email" placeholder="Email" />
-          <input className="input" type="password" placeholder="Password" />
+      <div className="bg-cool-grey-050 py-5 px-14 rounded-xl flex flex-col gap-2.5 items-center ">
+        <h1 className="font-bold text-2xl">Gossip Chat</h1>
+        <h3 className="text-xl">Register</h3>
+        <form
+          className="flex flex-col gap-4 max-w-[360px]"
+          onSubmit={handleSubmit}
+        >
+          <input className="forminput" type="text" placeholder="Name" />
+          <input className="forminput" type="email" placeholder="Email" />
+          <input className="forminput" type="password" placeholder="Password" />
           <input
             className="input"
             style={{ display: "none" }}
             type="file"
             id="file"
+            onChange={handleImage}
           />
+
           <label
-            className="flex items-center gap-2.5  text-xs cursor-pointer"
+            className="flex items-center gap-2.5 text-xs cursor-pointer"
             htmlFor="file"
           >
             <img className="w-10" src={Add} alt="Add" />
-            <span> Add an avatar</span>
+
+            {image && (
+              <img
+                src={image}
+                alt="avatar"
+                className="w-20 mx-auto rounded-full"
+              />
+            )}
+            {!image && <span> Add an avatar</span>}
           </label>
 
           <button
-            className="bg-indigo-600 flex items-center justify-center gap-1 text-white p-2.5 font-bold cursor-pointer rounded-lg hover:bg-indigo-400 "
+            className="bg-indigo-600 flex items-center justify-center gap-1 text-white p-2.5 font-bold cursor-pointer rounded-lg hover:bg-indigo-500 "
             disabled={loading}
           >
-            <div className="w-4 h-4 border-b-2 border-b-white animate-spin rounded-full"></div>
             Sign up
           </button>
           {loading && "Uploading and compressing the image please wait..."}
           {err && <span>Something went wrong</span>}
         </form>
 
-        <p className=" text-xs mt-2.5">
-          You do have an account? <Link to="/login">Login</Link>
+        <p className="text-xs mt-2.5">
+          You do have an account?{" "}
+          <Link className="text-indigo-400" to="/login">
+            Login
+          </Link>
         </p>
       </div>
     </div>
