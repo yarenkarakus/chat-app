24d7be3262796eb7d594528e9439cc34630a74fe
Yaren Karakus <yarenkarakus007@gmail.com>
2023-01-30 14:24:41 +0300

    refactor: Link color, button hover color  changed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,20 +21,23 @@ const Login = () => {
   };
 
   return (
-    <div className="  h-screen flex items-center justify-center">
+    <div className=" h-screen flex items-center justify-center">
       <div className="bg-cool-grey-050 py-5 px-14 rounded-lg flex flex-col gap-2.5 items-center ">
-        <h1 className=" font-bold text-2xl">Gossip Chat</h1>
-        <h3 className="  text-xl">Login</h3>
+        <h1 className="font-bold text-2xl">Gossip Chat</h1>
+        <h3 className="text-xl">Login</h3>
         <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-          <input className="input" type="email" placeholder="email" />
-          <input className="input" type="password" placeholder="password" />
-          <button className="bg-indigo-600 text-white p-2.5 font-bold cursor-pointer rounded-lg hover:bg-indigo-400">
+          <input className="loginput" type="email" placeholder="email" />
+          <input className="loginput" type="password" placeholder="password" />
+          <button className="bg-indigo-600 text-white p-2.5 font-bold cursor-pointer rounded-lg hover:bg-indigo-500">
             Sign in
           </button>
           {err && <span>Something went wrong</span>}
         </form>
         <p className=" text-xs mt-2.5">
-          You don't have an account? <Link to="/register">Register</Link>
+          You don't have an account?{" "}
+          <Link className="text-indigo-400" to="/register">
+            Register
+          </Link>
         </p>
       </div>
     </div>
