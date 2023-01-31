5d4022d5cb1769f10572d2c31b9f0804465f0755
Yaren Karakus <yarenkarakus007@gmail.com>
2023-01-31 14:00:56 +0300

    feat: loading animation

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -12,14 +12,17 @@ import {
 } from "firebase/firestore";
 import { db } from "../firebase";
 import { AuthContext } from "../context/AuthContext";
+
 const Search = () => {
   const [username, setUsername] = useState("");
   const [user, setUser] = useState(null);
   const [err, setErr] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const { currentUser } = useContext(AuthContext);
 
   const handleSearch = async () => {
+    setLoading(true);
     const q = query(
       collection(db, "users"),
       where("displayName", "==", username)
@@ -29,9 +32,11 @@ const Search = () => {
       const querySnapshot = await getDocs(q);
       querySnapshot.forEach((doc) => {
         setUser(doc.data());
+        setErr(false);
       });
     } catch (err) {
       setErr(true);
+      setLoading(false);
     }
   };
 
@@ -40,19 +45,16 @@ const Search = () => {
   };
 
   const handleSelect = async () => {
-    //check whether the group(chats in firestore) exists, if not create
     const combinedId =
       currentUser.uid > user.uid
         ? currentUser.uid + user.uid
         : user.uid + currentUser.uid;
     try {
+      setErr(false);
       const res = await getDoc(doc(db, "chats", combinedId));
-
       if (!res.exists()) {
-        //create a chat in chats collection
         await setDoc(doc(db, "chats", combinedId), { messages: [] });
 
-        //create user chats
         await updateDoc(doc(db, "userChats", currentUser.uid), {
           [combinedId + ".userInfo"]: {
             uid: user.uid,
@@ -72,24 +74,32 @@ const Search = () => {
         });
       }
     } catch (err) {}
-
+    setLoading(false);
+    setErr(true);
     setUser(null);
     setUsername("");
   };
   return (
     <div className="border-b-[1px] border-b-solid border-b-cool-grey-200">
-      <div className="searchform bg-indigo-900">
+      <div className="searchform bg-indigo-900 flex flex-row shadow-inner  shadow-blue-500/50">
         <input
-          className="w-full bg-transparent shadow-inner rounded-lg  shadow-blue-500/50 border-none  text-white outline-none placeholder:text-cool-grey-100 p-2.5"
+          className="w-full bg-transparent rounded-lg border-none text-white outline-none placeholder:text-cool-grey-100 p-2.5"
           type="text"
           placeholder="Find a user"
           onKeyDown={handleKey}
           onChange={(e) => setUsername(e.target.value)}
           value={username}
         />
-      </div>
 
-      {err && <span>User not found!</span>}
+        {loading && (
+          <div
+            className="animation m-2.5 p-2.5 animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-orange-vivid-400 rounded-full"
+            role="status"
+          ></div>
+        )}
+      </div>
+      {err && <span className="text-white">User not found!</span>}
+
       {user && (
         <div
           className="p-2.5 flex items-center gap-2.5 text-white cursor-pointer hover:bg-indigo-900"
